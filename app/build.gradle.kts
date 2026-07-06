886341944aa533639488bea69f72be500b868afa
caliverX <night.furyali79@gmail.com>
2026-07-06 12:24:06 +0200

    new edit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.14.0")
-    implementation("com.google.android.material:material:1.11.0")
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
